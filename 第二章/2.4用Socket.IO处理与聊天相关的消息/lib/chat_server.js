49bd855b625c1cfa4d082c447872d7a45b41e977
adolph <1138027478@qq.com>
2017-11-24 01:46:31 +0800

    add 添加修改昵称的逻辑

--- 第二章/2.4用Socket.IO处理与聊天相关的消息/lib/chat_server.js
+++ 第二章/2.4用Socket.IO处理与聊天相关的消息/lib/chat_server.js
@@ -15,12 +15,14 @@ exports.listen = function(server){
     io = socket.listen(server);
 
     socket.on('connection',function(client){
-
+        // 分布昵称
         guestRoomNumber = handlerAcceptNickName(client,nickNames,nameUsed,guestNumber);
-
+        // 加入房间
         joinRoom(client,'Guest');
-
+        // 修改昵称
+        handlerChangeNickName(client,nickNames,nameUsed);
     });
+
     /**
      * 设置用户的昵称
      * @param {*} socket 
@@ -44,8 +46,9 @@ exports.listen = function(server){
             return guestNumber + 1;
         }
     }
+
     /**
-     * 加入房间.
+     * 加入房间.并发布消息.
      * @param {*} socket
      */
     function joinRoom(socket,room){
@@ -57,10 +60,59 @@ exports.listen = function(server){
         socket.broadcast.to(room).emit('message',{
             'text':nickNames[socket.io] + '加入房间,房间号为:' + room
         });
-        // 开始获取改房间下的所有用户
+        // 开始获取改房间下的所有用户 老版本:io.sockets.clients(room);
         var users = socket.of('/').in(room).clients;
         if(users.length > 1){
-            // 获取所有用户的昵称,别发布出去.
+            // 获取所有用户的昵称,一起发布出去.
+            var usersInRoomSummary = '用户:' + nickNames[socket.io];
+            for(var index in users){
+                var socketid =  users[index].id;
+                if(socket.id != socketid){
+                    if(index > 0){
+                        usersInRoomSummary += ',';
+                    }else{
+                        usersInRoomSummary += nickNames[socketid];
+                    }
+                }
+            }
+            // 触发客户端的message事件.
+            socket.emit('message',{
+                'text':usersInRoomSummary
+            });
         }
+    }
+
+    /**
+     * 修改昵称
+     * @param {*} socket 
+     * @param {*} nickNames 
+     * @param {*} nameUsed 
+     */
+    function handlerChangeNickName(socket,nickNames,nameUsed){
+        // 判断昵称是否存在
+        socket.on('nick',function(nickname){
+            // 如果存在则直接报错
+            if(nickNames.indexOf(nickname) > -1){
+                socket.emit('nameResult',{
+                    'success':false,
+                    'message':'名称被占用了'
+                });
+            }else{
+                // 获取以前的昵称
+                var prevName = nickNames[socket.id];
+                // 找到对应的索引
+                var prevNameIndex = nameUsed.indexOf(prevName);
+                // 删除索引
+                delete nameUsed[index];
+                // 新增昵称
+                nameUsed.push(nickname);
+                // 修改昵称
+                nickNames[socket.id] = nickname;
+                socket.emit('nameResult',{
+                    'success':true,
+                    'name':nickname
+                });
+            }
+        });
     }
 }
