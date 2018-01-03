a33b7a50f0a0c21a48bb0ff125ecca281f2a1bd0
adolph <1138027478@qq.com>
2018-01-03 10:03:21 +0800

    fix 修复7.1.4的代码

--- 第七章/7.1解析cookie请求主题和查询字符串的中间件/7.1.4query查询字符串解析/index.js
+++ 第七章/7.1解析cookie请求主题和查询字符串的中间件/7.1.4query查询字符串解析/index.js
@@ -1,6 +1,7 @@
 // 由于新版本已经将query丢弃改用qs,以下是qs的版本.
 var qs = require('qs');
 var connect = require('connect');
+var url = require('url');
 var bodyParser = require('body-parser');
 connect()
   .use(bodyParser())
@@ -9,6 +10,8 @@ connect()
     // console.dir(qs.parse(req.query,{ ignoreQueryPrefix: true }));
     // undefined
     // req.query
-    console.dir(req.query);
+    // 拿到解析的字符串.暂时不知道如何从request中获取,只能先获取url,然后用url模块的parse方法解析一下
+    var query = url.parse(req.url);
+    console.dir(qs.parse(query.query));
   })
   .listen(3000);
